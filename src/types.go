90746b247d760c065efd6a5cf601dd9bed699136
4n4k1n <anakin@pregitzer.de>
2025-08-17 19:56:18 +0200

    created new struct for the stats data

--- src/types.go
+++ src/types.go
@@ -20,3 +20,15 @@ type Data struct {
 	src_port uint16
 	dst_port uint16
 }
+
+type Stats_data struct {
+	total_packets     int
+	captured_duration int
+	average_rate      float32
+	unique_src_ids    int
+	unique_dst_ids    int
+	tcp_packets       int
+	udp_packets       int
+	most_active_src   string
+	most_active_dst   string
+}
