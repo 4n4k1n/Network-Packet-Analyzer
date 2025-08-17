9e40b78efe892a0003ca245c7ac62b9b115f0d52
4n4k1n <anakin@pregitzer.de>
2025-08-17 20:07:05 +0200

    start fillinjg the stats struct

--- src/main.go
+++ src/main.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	var err error
 	var handle *pcap.Handle
-	var count int32 = 0
 	startTime := time.Now()
 	var data Data
 	var stats_data Stats_data
@@ -30,12 +29,14 @@ func main() {
 
 	pack_src := gopacket.NewPacketSource(handle, handle.LinkType())
 	for pack := range pack_src.Packets() {
-		count++
+		stats_data.total_packets++
 		data = getData(pack)
 		printPacketData(data)
 		if time.Since(startTime) > time.Duration(*parse_data.duration)*time.Second {
 			break
 		}
 	}
+	stats_data.captured_duration = *parse_data.duration
+	stats_data.average_rate = float32(stats_data.total_packets) / float32(stats_data.captured_duration)
 	printStats(stats_data)
 }
