456ff42aff527e6e19f942abd54819a95fdb0f92
4n4k1n <anakin@pregitzer.de>
2025-08-17 20:07:23 +0200

    updated print format

--- src/prints.go
+++ src/prints.go
@@ -7,17 +7,17 @@ import (
 )
 
 func printStats(stats_data Stats_data) {
-	fmt.Printf("=== Capture Summary ===\n")
+	fmt.Printf("\n=== Capture Summary ===\n")
 	fmt.Printf("Total packets captured  : %d\n", stats_data.total_packets)
 	fmt.Printf("Capture duration        : %d\n", stats_data.captured_duration)
-	fmt.Printf("Average rate            : %f\n", stats_data.average_rate)
+	fmt.Printf("Average rate            : %.1f\n", stats_data.average_rate)
 	fmt.Printf("Unique source IPs       : %d\n", stats_data.unique_src_ids)
 	fmt.Printf("Unique destination IPs  : %d\n", stats_data.unique_dst_ids)
-	fmt.Printf("Protocol breakdown      :\n")
-	fmt.Printf("TCP : %d packets (%.1f)", stats_data.tcp_packets, percent.PercentOf(stats_data.tcp_packets, stats_data.total_packets))
-	fmt.Printf("UDP : %d packets (%.1f)", stats_data.udp_packets, percent.PercentOf(stats_data.udp_packets, stats_data.total_packets))
+	fmt.Printf("Protocol breakdown\n")
+	fmt.Printf("   TCP : %d packets (%.1f)\n", stats_data.tcp_packets, percent.PercentOf(stats_data.tcp_packets, stats_data.total_packets))
+	fmt.Printf("   UDP : %d packets (%.1f)\n", stats_data.udp_packets, percent.PercentOf(stats_data.udp_packets, stats_data.total_packets))
 	fmt.Printf("Most active source      : %s (%d packets)\n", stats_data.most_active_src, 0)
-	fmt.Printf("Most active destination : %s (%d packets)\n", stats_data.most_active_dst, 0)
+	fmt.Printf("Most active destination : %s (%d packets)\n\n", stats_data.most_active_dst, 0)
 }
 
 func printHeaderLine() {
@@ -25,5 +25,5 @@ func printHeaderLine() {
 }
 
 func printPacketData(data Data) {
-			fmt.Printf("%-15s  %-15s  %-8s  %-10d  %-10d\n", data.src_ip, data.dst_ip, data.protocol, data.src_port, data.dst_port)
+	fmt.Printf("%-15s  %-15s  %-8s  %-10d  %-10d\n", data.src_ip, data.dst_ip, data.protocol, data.src_port, data.dst_port)
 }
