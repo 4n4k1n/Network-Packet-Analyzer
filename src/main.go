347fd67586e8eaa54d4885ee4b35bb6e26071850
4n4k1n <anakin@pregitzer.de>
2025-08-17 19:57:06 +0200

    addedthe new print functions

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
@@ -15,6 +14,7 @@ func main() {
 	var count int32 = 0
 	startTime := time.Now()
 	var data Data
+	var stats_data Stats_data
 
 	parse_data := parse()
 
@@ -26,18 +26,16 @@ func main() {
 
 	filterInput(parse_data, handle)
 
-	fmt.Printf("%-15s  %-15s  %-8s  %-10s  %-10s\n", "src IP", "test IP", "protocol", "src port", "dest port")
+	printHeaderLine()
 
 	pack_src := gopacket.NewPacketSource(handle, handle.LinkType())
 	for pack := range pack_src.Packets() {
 		count++
 		data = getData(pack)
-		fmt.Printf("%-15s  %-15s  %-8s  %-10d  %-10d\n", data.src_ip, data.dst_ip, data.protocol, data.src_port, data.dst_port)
-		// fmt.Printf("srcIP: %s, DstIP: %s, Prot: %s\n", data.src_ip, data.dst_ip, data.protocol)
+		printPacketData(data)
 		if time.Since(startTime) > time.Duration(*parse_data.duration)*time.Second {
 			break
 		}
 	}
-
-	// fmt.Printf("Network interface: %s\nPackets captured: %d\n", device, count)
+	printStats(stats_data)
 }
